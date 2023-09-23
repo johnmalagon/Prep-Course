34a16716ac191867c1f237c94c1f4198d556aff5
johnMalagon <johnalexandermalagongarcia@gmail.com>
2023-09-23 12:06:11 -0500

    Simulacion Challenge

---
+++ Simulación Challenge/WILL-main/06-07-08.js
@@ -0,0 +1,113 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function crearClasePersona() {
+  class Persona {
+    constructor(nombre, edad, hobbies, amigos) {
+      // El constructor de la clase Persona recibe nombre (string), edad (integer), hobbies (array de strings), amigos (array de objetos)
+      // Inicializar las propiedades de la persona con los valores recibidos como argumento
+
+      // Tu código aca:
+      if (typeof nombre === 'string'){
+        this.nombre = nombre;
+      }
+      if (typeof edad === 'number'){
+        this.edad = edad;
+      }
+      if (Array.isArray(hobbies)){
+        this.hobbies = hobbies;
+      }
+      if (Array.isArray(amigos)){
+        this.amigos = amigos;
+      }
+
+
+    }
+
+    addFriend(nombre, edad) {
+      // El método 'addFriend' recibe un string 'nombre' y un entero 'edad' y debe agregar un objeto:
+      // { nombre: nombre, edad: edad} al arreglo de amigos de la persona.
+      // No debe retornar nada.
+
+      // Tu código aca:
+      Persona.prototype.addFriend = function(nombre, edad){
+        this.amigos.push({nombre: nombre, edad: edad});
+      }
+
+
+    }
+
+    addHobby(hobby) {
+      // El método 'addHobby' recibe un string 'hobby' y debe agregarlo al arreglo de hobbies de la persona.
+      // No debe retornar nada.
+
+      // Tu código aca:
+      this.addHobby = function(hobby){
+        this.hobbies.push(hobby);
+      }
+
+
+
+    }
+    getFriends() {
+      // El método 'getFriends' debe retornar un arreglo con sólo los nombres del arreglo de amigos
+      // de la persona.
+      // Ej:
+      // Suponiendo que la persona tiene estos amigos: [{nombre: 'martin', edad: 31},{nombre: 'toni', edad: 33}]
+      // persona.getFriends() debería devolver ['martin', 'toni']
+
+      // Tu código aca:
+      this.getFriends = function(){
+        return this.amigos.map(function(amigo){
+          return amigo.nombre;
+        })
+      }
+
+    }
+
+    getHobbies() {
+      // El método 'getHobbies' debe retornar un arreglo con los hobbies de la persona
+      // Ej:
+      // persona.getHobbies() debe devolver ['correr', 'dormir', 'nadar']
+
+      // Tu código aca:
+      this.getHobbies = function(){
+        return this.hobbies
+      }
+
+    }
+
+    getPromedioEdad() {
+      // El método 'getPromedioEdad' debe retornar el promedio de edad de los amigos de una persona
+      // Ej:
+      // Si la persona tuviera estos amigos:
+      // {
+      //   amigos: [{
+      //     nombre: 'toni',
+      //     edad: 33,
+      //   }, {
+      //     nombre: 'Emi',
+      //     edad: 25
+      //   }]
+      // }
+      // persona.getPromedioEdad() debería devolver 29 ya que (33 + 25) / 2 = 29
+
+      // Tu código aca:
+      this.getPromedioEdad = function(){
+        return this.amigos.reduce(function(acumulador, amigo){
+          return acumulador + amigo.edad;
+        }, 0) / this.amigos.length
+      }
+      
+    }
+  };
+
+  return Persona;
+}
+
+// No modifiques nada debajo de esta linea //
+
+module.exports = crearClasePersona
